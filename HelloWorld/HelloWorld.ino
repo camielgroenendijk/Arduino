f9613308abcde8b3d563785e44e748659c7cf51f
Camiel <camiel1998@gmail.com>
2020-09-17 10:08:30 +0200

    Added 'Helloworld' Project

---
+++ HelloWorld/HelloWorld.ino
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+U8G2_SSD1306_128X64_NONAME_1_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 16, /* data=*/ 17);   // ESP32 Thing, HW I2C with pin remapping
+
+
+
+void setup(void) {
+  u8g2.begin();  
+}
+
+void loop(void) {
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+    u8g2.drawStr(0,24,"Hello World!");
+    u8g2.drawStr(0,24,"How You Doin?");
+  } while ( u8g2.nextPage() );
+    delay(1000);
+}
